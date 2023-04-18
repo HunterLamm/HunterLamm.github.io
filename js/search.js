134df12fec0cf9c82f8f1e6f49d61eef032a4ed2
HunterLamm <hunterlzh12@gmail.com>
2023-04-18 14:20:31 -0700

    Site updated: 2023-04-18 14:20:31

--- js/search.js
+++ js/search.js
@@ -62,6 +62,7 @@ var searchFunc = function(path, search_id, content_id) {
                         // show search results
                         if (isMatch) {
                             str += "<li><a href='" + data_url + "' class='search-result-title'>" + data_title + "</a>";
+                            /*
                             var content = data.content.trim().replace(/<[^>]+>/g, "");
                             if (first_occur >= 0) {
                                 // cut out 100 characters
@@ -90,6 +91,7 @@ var searchFunc = function(path, search_id, content_id) {
 
                                 str += "<p class=\"search-result\">" + match_content + "...</p>"
                             }
+                            */
                             str += "</li>";
                         }
                     });
